ce0bfb37cfa5fc2a701cb2309c089f119e910483
tomelsom <te28@hotmail.com>
2024-01-08 18:55:34 +0000

    updateing add function

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -2,38 +2,53 @@
 @section('content')
   <main class="container">
     <section>
-      <div class="titlebar">
-          <h1>Add Product</h1>
-          <button>Save</button>
-      </div>
-      <div class="card">
-          <div>
-              <label>Name</label>
-              <input type="text" >
-              <label>Description (optional)</label>
-              <textarea cols="10" rows="5" ></textarea>
-              <label>Add Image</label>
-              <img src="" alt="" class="img-product" />
-              <input type="file" >
-          </div>
-          <div>
-              <label>Category</label>
-              <select  name="" id="" >
-                  <option value="" >Email Subscription</option>
-              </select>
-              <hr>
-              <label>Inventory</label>
-              <input type="text" class="input" >
-              <hr>
-              <label>Price</label>
-              <input type="text" class="input" >
-          </div>
-      </div>
-      <div class="titlebar">
-          <h1></h1>
-          <button>Save</button>
-      </div>
+      <form method="post" action="{{route('products.store')}}" enctype="multipart/form-data">
+        @csrf
+        <div class="titlebar">
+            <h1>Add Product</h1>
+            <button>Save</button>
+        </div>
+        <div class="card">
+            <div>
+                <label>Name</label>
+                <input type="text" name="name">
+                <label>Description (optional)</label>
+                <textarea cols="10" rows="5" name="description" ></textarea>
+                <label>Add Image</label>
+                <img src="" alt="" class="img-product" id="file-preview" />
+                <input type="file" name="image" accept="image/*" onchange="showFile(event)">
+            </div>
+            <div>
+                <label>Category</label>
+                <select  name="" id="" >
+                    <option value="" >Email Subscription</option>
+                </select>
+                <hr>
+                <label>Inventory</label>
+                <input type="text" class="input" >
+                <hr>
+                <label>Price</label>
+                <input type="text" class="input" >
+            </div>
+        </div>
+        <div class="titlebar">
+            <h1></h1>
+            <button>Save</button>
+        </div>
     </section>
 
   </main>
+  <script>
+    function showFile(event){
+      var input = event.target;
+      var reader = new FileReader();
+      reader.onload = function(){
+        var dataURL = reader.result;
+        var output = document.getElementById('file-preview');
+        output.src = dataURL;
+      };
+      reader.readAsDataURL(input.files[0]);
+    }
+
+  </script>
 @endsection
