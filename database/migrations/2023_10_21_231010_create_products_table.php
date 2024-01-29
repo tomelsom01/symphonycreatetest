bde2e3732fb406e8e5a181a84b2a2bd583d0e3a1
tomelsom <te28@hotmail.com>
2024-01-29 13:44:26 +0000

    correcting database

--- database/migrations/2023_10_21_231010_create_products_table.php
+++ database/migrations/2023_10_21_231010_create_products_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable();
+            $table->text('description')->nullable();
+            $table->string('category')->nullable();
+            $table->integer('quantity')->nullable();
+            $table->integer('price')->nullable();
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
